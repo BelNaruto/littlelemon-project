c1cf680e778b21669d217dd3012bb0becca98f8c
Belchior Pinto <belchiorarkad@gmail.com>
2023-11-17 09:06:47 +0000

    message

--- LittlelemonAPI/throttles.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.throttling import UserRateThrottle
-
-# For different endpoints calls, some endpoints can get 10 per min for example
-class TenCallsPerMinute(UserRateThrottle):
-    scope='ten'
